0bf120b8e39983db6f34061aadc802477d5cc029
Michael H <michael@michaelhall.tech>
2023-09-29 17:49:27 -0400

    versioning

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -9,4 +9,4 @@ Copyright (C) 2023 Michael Hall <https://github.com/mikeshardmind>
 from .scheduler import DiscordBotScheduler, ScheduledDispatch, Scheduler
 
 __all__ = ["DiscordBotScheduler", "ScheduledDispatch", "Scheduler"]
-__version__ = "2023.08.29"
+__version__ = "2023.09.29"
